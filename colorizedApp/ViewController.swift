90769b267bad79067b19d9b55d93a958b196c8ac
DruzhArt <druzhart90@gmail.com>
2022-09-25 20:15:40 +0300

    setup

--- colorizedApp/ViewController.swift
+++ colorizedApp/ViewController.swift
@@ -11,16 +11,26 @@ class ViewController: UIViewController {
 
    // MARK: - IBOutlet
     @IBOutlet var changeColor: UIView!
-    
-    
-    
+    @IBOutlet var redLabel: UILabel!
+//    @IBOutlet var blueLabel: UILabel!
+//    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var sliderRed: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         changeColor.layer.cornerRadius = 30
+//        changeColor.backgroundColor = UIColor(displayP3Red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        setupSlider()
     }
 
-
+    // MARK: - IBAction
+    
+    @IBAction func setupSlider() {
+        sliderRed.minimumTrackTintColor = .red
+        redLabel.text = sliderRed.value.formatted()
+        
+    }
+    
 }
 
 
